81ca5d0a51fa0ff8459dcc2c06f8b253639decb4
temptedwithouta <danielsulistio125@gmail.com>
2023-12-04 14:19:52 +0700

    Model

--- src/model/OrderItem.java
+++ src/model/OrderItem.java
@@ -28,4 +28,17 @@ public class OrderItem {
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
+
+    public static void createOrderItem(int orderId, int menuItemId, int quantity) {
+        String query = "INSERT INTO order_items (order_id, menu_item_id, order_item_quantity) VALUES (?, ?, ?)";
+        try (Connection connection = Connect.getInstance().getConnection();
+                PreparedStatement ps = connection.prepareStatement(query)) {
+            ps.setInt(1, orderId);
+            ps.setInt(2, menuItemId);
+            ps.setInt(3, quantity);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
